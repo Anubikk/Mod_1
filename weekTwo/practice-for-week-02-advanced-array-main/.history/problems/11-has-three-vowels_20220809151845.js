ebf7dd3c8e5e4fa7ef1e2d9e9f93530a9855c5e2
Hayden Woods <anubikk@gmail.com>
2022-08-09 16:40:57 -0500

    Advanced_Array

---
+++ weekTwo/practice-for-week-02-advanced-array-main/.history/problems/11-has-three-vowels_20220809151845.js
@@ -0,0 +1,43 @@
+/*
+Write a function `hasThreeVowels` that accepts a string as an argument.
+The function should return a boolean indicating whether or not the string
+contains at least three different vowels.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
+
+*/
+
+let hasThreeVowels = function(string) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let i = 0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            let count = 0;
+            for (let j = 0; j < string.length; j++) {
+                if (vowels.includes(string[j])) {
+                    count++;
+                }
+            }
+            if (count >= 3) {
+                return true;
+            } else 
+        }
+    }
+
+// Your code here
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = hasThreeVowels;
+} catch (e) {
+    module.exports = null;
+}
---
+++ weekTwo/practice-for-week-02-advanced-array-main/.history/problems/17-repeating-translate_20220809163519.js
@@ -0,0 +1,78 @@
+/*
+Write a function `repeatingTranslate` that accepts a sentence as an argument.
+The function should translate the sentence according to the following rules:
+
+- words that are shorter than 3 characters are unchanged
+- words that are 3 characters or longer are translated according to the
+  following rules:
+  - if the word ends with a vowel, simply repeat the word twice (example:
+    'like'->'likelike')
+  - if the word ends with a non-vowel, repeat all letters that come after the
+    word's last vowel, including the last vowel itself (example:
+    'trash'->'trashash')
+
+Note that if words are capitalized in the original sentence, they should remain
+capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Can you reduce the problem into helper functions?
+
+Examples:
+
+console.log(repeatingTranslate("we like to go running fast"));  // "we likelike to go runninging fastast"
+console.log(repeatingTranslate("he cannot find the trash"));    // "he cannotot findind thethe trashash"
+console.log(repeatingTranslate("pasta is my favorite dish"));   // "pastapasta is my favoritefavorite dishish"
+console.log(repeatingTranslate("her family flew to France"));   // "herer familyily flewew to FranceFrance"
+
+*/
+
+let repeatingTranslate = function(sentence) {
+    let words = sentence.split(' ');
+    let newSentence = '';
+    words.forEach(function(word) {
+        if (word.length < 3) {
+            newSentence += word + ' ';
+        } else {
+            let newWord = '';
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u') {
+                    newWord += word[i] + word[i];
+                } else {
+                    newWord += word[i];
+                }
+            }
+            newSentence += newWord + ' ';
+        }
+    } );
+    return newSentence;
+    
+
+
+
+let translateWord = function(word) {
+     let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (vowels.includes(word[i])) {
+            count++;
+        }
+    }
+    if (count >= 3) {
+        return word + word;
+    } else {
+            return word;
+            }
+};
+
+
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = repeatingTranslate;
+} catch (e) {
+    module.exports = null;
+}
